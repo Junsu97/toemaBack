0192b3726c3bc95cc7075a4b04209afb8b1957d7
junsu97 <wnstn7759@naver.com>
2024-01-09 18:05:06 +0900

    Create UserService

---
+++ toemaUserService/src/main/java/junsu/personal/ToemaUserServiceApplication.java
@@ -0,0 +1,13 @@
+package junsu.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToemaUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToemaUserServiceApplication.class, args);
+    }
+
+}
---
+++ toemaUserService/src/test/java/junsu/personal/ToemaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package junsu.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToemaUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
