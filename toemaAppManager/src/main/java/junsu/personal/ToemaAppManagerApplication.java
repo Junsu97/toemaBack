c59e0abf0d5ca930ca4b745f2de790d5adcc7fe6
junsu97 <wnstn7759@naver.com>
2024-01-09 16:53:41 +0900

    Create AppManager

---
+++ toemaAppManager/src/main/java/junsu/personal/ToemaAppManagerApplication.java
@@ -0,0 +1,13 @@
+package junsu.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToemaAppManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToemaAppManagerApplication.class, args);
+    }
+
+}
