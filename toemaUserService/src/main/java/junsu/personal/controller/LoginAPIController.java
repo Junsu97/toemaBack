cc49ec8898ad015fb39fc2bbe305d14ba9ddf223
junsu97 <wnstn7759@naver.com>
2024-01-30 14:54:17 +0900

    1. LoginAPIController 작성 끝 2. Redis 의존성 추가

--- toemaUserService/src/main/java/junsu/personal/controller/LoginAPIController.java
+++ toemaUserService/src/main/java/junsu/personal/controller/LoginAPIController.java
@@ -46,11 +46,11 @@ public class LoginAPIController {
     )
     @PostMapping(value = "loginSucces")
     public MsgDTO loginSuccess(@AuthenticationPrincipal AuthInfo authInfo,
-                               HttpServletResponse response){
+                               HttpServletResponse response) {
         log.info(this.getClass().getName() + ".loginSuccess Start!!!!");
 
         UserInfoDTO rDTO = Optional.ofNullable(authInfo.userInfoDTO())
-                        .orElseGet(() -> UserInfoDTO.builder().build());
+                .orElseGet(() -> UserInfoDTO.builder().build());
 
         String userId = CmmUtil.nvl(rDTO.userId());
         String userName = CmmUtil.nvl(rDTO.userName());
@@ -66,13 +66,13 @@ public class LoginAPIController {
         log.info("accessToken : " + accessToken);
 
         ResponseCookie cookie = ResponseCookie.from(accessTokenName, accessToken)
-                        .domain("localhost")
-                        .path("/")
-                                .secure(true)
-                                .sameSite("None")
-                        .maxAge(accessTokenValidTime)
-                        .httpOnly(true)
-                        .build();
+                .domain("localhost")
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .maxAge(accessTokenValidTime)
+                .httpOnly(true)
+                .build();
 
         // 기존 쿠기 모두 삭제하고 Cokie에 Access 토큰 저장
         response.setHeader("Set-Cookie", cookie.toString());
@@ -84,6 +84,45 @@ public class LoginAPIController {
         // Refresh Token은 보안상 노출되면 위험하기 때문에 Refresh Tokne은 DB에 저장하고
         // DB를 조회하기 위한 값만 Refresh Token으로 생성
         // Refresh Token은 Acess Token에 비해 만료시간을 길게 설정한다.
+        String refreshToken = jwtTokenProvider.createToken(userId, userRoles, JwtTokenType.REFRESH_TOKEN);
+        log.info("refreshToken : " + refreshToken);
+
+        cookie = ResponseCookie.from(refreshTokenName, refreshToken)
+                .domain("localhost") // 배포 후 변경
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .maxAge(refreshTokenValidTime) // JWT Refresh Token 만료시간 설정
+                .httpOnly(true)
+                .build();
+
+        // 기존 쿠키에 Refresh Token 저장하기
+        // 여기 레디스에 저장하는 코드로 바꿔야함
+        response.addHeader("Set-Cookie", cookie.toString());
+
+        // 결과 메시지 전달하기
+        MsgDTO dto = MsgDTO.builder().result(1).msg(userName + "님 로그인이 되었습니다.").build();
+
         log.info(this.getClass().getName() + ".loginSuccess End!!!!");
+
+        return dto;
+    }
+
+    @Operation(summary = "로그인 실패처리  API", description = "로그인 실패처리 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Page Not Found!"),
+            }
+    )
+    @PostMapping(value = "loginFail")
+    public MsgDTO loginFail(){
+        log.info(this.getClass().getName() + ".loginFail Start!!");
+
+        // 결과 메시지 전달하기
+        MsgDTO dto = MsgDTO.builder().result(1).msg("로그인이 실패하였습니다.").build();
+
+        log.info(this.getClass().getName() + ".logfinFail End!!!");
+
+        return dto;
     }
 }
