4c81586c46030c4dc4a9bcef30ef389116b0f8b6
junsu97 <wnstn7759@naver.com>
2024-03-07 20:46:05 +0900

    .

---
+++ BackingService/src/main/java/junsu/personal/dto/TokenDTO.java
@@ -0,0 +1,10 @@
+package junsu.personal.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+
+@Builder
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public record TokenDTO(String userId,
+                       String role) {
+}
