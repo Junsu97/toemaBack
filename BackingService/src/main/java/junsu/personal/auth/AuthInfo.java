f36278cc9103c3cf287402b6d6a515c2f998aae2
junsu97 <wnstn7759@naver.com>
2024-03-07 21:27:23 +0900

    .

---
+++ BackingService/src/main/java/junsu/personal/auth/AuthInfo.java
@@ -0,0 +1,67 @@
+package junsu.personal.auth;
+
+import junsu.personal.dto.UserInfoDTO;
+import junsu.personal.util.CmmUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+public record AuthInfo(UserInfoDTO userInfoDTO) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        Set<GrantedAuthority> pSet = new HashSet<>();
+
+        String roles = CmmUtil.nvl(userInfoDTO.roles());
+
+        log.info("getAuthorities / roles : " + roles );
+        if(roles.length() > 0){
+            for(String role : roles.split(",")){
+                pSet.add(new SimpleGrantedAuthority(role));
+            }
+        }
+
+        return pSet;
+    }
+
+    /**
+     * 사용자의 아이디를 Unique한 값으로 반환
+     * */
+    @Override
+    public String getUsername(){
+        return CmmUtil.nvl(userInfoDTO.userId());
+    }
+    // 사용자의 password를 반환
+    @Override
+    public String getPassword(){
+        return CmmUtil.nvl(userInfoDTO.password());
+    }
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired(){
+        return true; // true -> 만료되지 않았음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked(){
+        return true; // true -> 잠금되지 않았음
+    }
+
+    // 패스워드의 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true; // true -> 만료되지 않았음
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled(){
+        return true; // true -> 사용가능
+    }
+}
